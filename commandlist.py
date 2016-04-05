f34a351e13a32fc01f9cd70ed3928685dba3f63f
joe199 <joe199ortiz@gmail.com>
2016-04-05 16:23:34 +0200

    ultim canvi

---
+++ commandlist.py
@@ -0,0 +1,17 @@
+
+
+class CommandList(object):
+    def __init__(self):
+        super(CommandList, self).__init__()
+        self.commands=[]
+
+
+    def next(self):
+        self.commands.pop(0)
+
+    def append(self, command):
+        self.commands.append(command)
+
+    def length(self):
+        return len(self.commands)
+        
