a4d022e45c2939a158808bdb99b5625a55d4de56
joe199 <joe199ortiz@gmail.com>
2016-04-05 16:32:40 +0200

    he arreglat l'error de les eles

--- test/test_commandlist.py
+++ test/test_commandlist.py
@@ -5,17 +5,17 @@ class TestCommandList(unittest.TestCase):
 
     def test_000_newqueue_length(self):
         cl = commandlist.CommandList()
-        self.assertEqual(c1.length(),0)
+        self.assertEqual(cl.length(),0)
 
     def test_001_newqueue_cantpop(self):
         cl = commandlist.CommandList()
-        self.assertEqual(IndexError,c1.next)
+        self.assertEqual(IndexError,cl.next)
 
 
     def test_002_push_then_pop(self):
         cl = commandlist.CommandList()
-        c1.append("Test")
-        c=c1.next()
+        cl.append("Test")
+        c=cl.next()
         self.assertEqual(c, "Test")
 
 if __name__=="__main__":
