df12603bb469a51f8190c41d38a31567d3a853a5
joe199 <joe199ortiz@gmail.com>
2016-04-05 15:56:39 +0200

    Hem creat l'estructura

--- ambrosio.py
+++ ambrosio.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env/python
 # _*_ coding: utf-8 _*_
 
+class Ambrosio(object):
+    '''Class for ambrosio personal digital butler
 
-if __name__ == __main__:
-    print "Here be dragons!"
+    Will run our ambrosio'''
+
+    def __init__(self):
+        super(Ambrosio, self).__init__()
+
+    def mainloop(self):
+        #While True:
+        #   command = get_command
+        #   do_command(command)
+        #   update
+        pass
+
+
+
+    if __name__ == __main__:
+        print "Here be dragons!"
