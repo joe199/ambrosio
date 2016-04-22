0bf03bb3ae9afc370cf9c2c48e349db497653a71
Joe_199 <joe199ortiz@gmail.com>
2016-04-22 16:33:02 +0200

    Funciona bé, ja sol funciona amb el meu telegram.

--- ambrosio/channels/Channel.py
+++ ambrosio/channels/Channel.py
@@ -1,8 +1,13 @@
+
+import json
+
 class Channel(object):
     """Channel class"""
-    def __init__(self, name):
+    def __init__(self, cfg, name):
         super(Channel, self).__init__()
         self.name = name
+        self.cfg = cfg
+        print json.dumps(self.cfg, indent=4)
 
     def respond(self, response):
         print "RESPONSE: ", response
