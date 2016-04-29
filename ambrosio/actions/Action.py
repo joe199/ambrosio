949a1dbba0a5d2230b80a0f76ca3f814420bf2db
Joe_199 <joe199ortiz@gmail.com>
2016-04-29 16:28:22 +0200

    Wake up pc funciona

--- ambrosio/actions/Action.py
+++ ambrosio/actions/Action.py
@@ -1,7 +1,7 @@
 class Action(object):
     """Action to be carried on by Ambrosio"""
     def __init__(self, cfg):
-        super(Action, self).__init__(cfg)
+        super(Action, self).__init__()
         self.cfg = cfg
 
     def do(self):
--- ambrosio/actions/WakeAction.py
+++ ambrosio/actions/WakeAction.py
@@ -1,5 +1,5 @@
 from Action import Action
-import subprocess
+from wakeonlan import wol
 
 class WakeAction(Action):
     """Wakes the computer"""
@@ -9,7 +9,13 @@ class WakeAction(Action):
 
     def do(self, command):
         print "Will wake up a computer ", " ".join(command)
-        return "ok"
+        ret = "no reconeguts"
+        for wor in command:
+            for pc in self.cfg["wol"]["pcs"]:
+                if wor == pc["nom"]:
+                    wol.send_magic_packet(pc["mac"])
+                    ret = "OK"
+        return ret
 
     def is_for_you(self, word):
         if word in self.triggers:
